a9a1d1b99424acaef896becbc085a9e52adfe451
Pablo <pablodfb98@gmail.com>
2020-06-19 21:47:58 -0400

    First dev commit: added basic templates and configurations in order to start working properly

--- ItHelper/Proyecto/ithelper_tec/ithelper_tec/wsgi.py
+++ ItHelper/Proyecto/ithelper_tec/ithelper_tec/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ithelper_tec.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ithelper_tec.configuracion.local')
 
 application = get_wsgi_application()
--- ItHelper/Proyecto/ithelper_tec/manage.py
+++ ItHelper/Proyecto/ithelper_tec/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ithelper_tec.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ithelper_tec.configuracion.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
